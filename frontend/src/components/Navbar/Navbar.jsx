ca5569316f40cef632764e360cf81b75ea23180b
Evannfries <evann.fries@yahoo.com>
2022-12-27 14:49:32 -0600

    navbar basics

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='wrapper'>
+        <div className='left'>
+          <div className='item'>
+            <img className='flags' src='/images/flags.png' alt=''/>
+            <ArrowDropDownIcon />
+          </div>
+        </div>
+        <div className='center'></div>
+        <div className='right'></div>
+      </div>
+    </div>
   )
 }
 
