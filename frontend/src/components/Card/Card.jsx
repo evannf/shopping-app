8685116774ec829be550683683ece0aa0fc88a3d
Evannfries <evann.fries@yahoo.com>
2022-12-30 12:08:22 -0600

    product card data passed

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -5,7 +5,17 @@ import { Link } from "react-router-dom";
 const Card = ({item}) => {
   return (
     <Link to = {`/product/${item.id}`}>
-        <div className='card'>Card</div>
+        <div className='card'>
+            <div className='image'>
+                <img src={item.img} alt='' className='firstImage' />
+                <img src={item.img2} alt='' className='secondImage' />
+            </div>
+            <h2>{item.title}</h2>
+            <div className='prices'>
+                <h3>${item.oldPrice}</h3>
+                <h3>${item.price}</h3>
+            </div>
+        </div>
     </Link>
 
   )
