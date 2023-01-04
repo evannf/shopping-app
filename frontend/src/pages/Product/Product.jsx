0bddf267968aa0edb94c3c10ec48206d1041e664
Evannfries <evann.fries@yahoo.com>
2023-01-04 14:56:30 -0600

    single product page layout

--- frontend/src/pages/Product/Product.jsx
+++ frontend/src/pages/Product/Product.jsx
@@ -1,8 +1,12 @@
 import { useState, React } from 'react';
-import "./Product.scss"
+import "./Product.scss";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import BalanceIcon from '@mui/icons-material/Balance';
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0)
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantitiy, setQuantitiy] = useState(1);
 
   const images = [
     "https://media.istockphoto.com/id/1152838910/photo/male-dark-blue-blazer-on-isolated-background.jpg?b=1&s=170667a&w=0&k=20&c=op8HLIk7SWCyUHJAJJ9XrcR0qVYntOBidNRhotcYKMs=",
@@ -20,7 +24,41 @@ const Product = () => {
           <img src={images[selectedImg]} alt='' />
         </div>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <h1>Title</h1>
+        <span>$35</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus ante ut varius vulputate. Vivamus ante dolor, tempus a elit vel, lacinia ornare du Duis sagittis dui vitae enim finibus tincidunt. In facilisis ex ut erat imperdiet, in volutpat lacus cursusi. Vivamus vel maximus felis. Nulla mattis egestas mollis.
+        </p>
+        <div className='quantity'>
+          <button onClick={() => setQuantitiy((prev) => prev === 1 ? 1 : prev-1)}>-</button>
+          {quantitiy}
+          <button onClick={() => setQuantitiy((prev) => prev+1)}>+</button>
+        </div>
+        <button className='add'>
+          <AddShoppingCartIcon /> Add to Cart
+        </button>
+        <div className='link'>
+          <div className='item'>
+            <FavoriteBorderIcon /> Add to Wishlist
+          </div>
+          <div className='item'>
+            <BalanceIcon /> Compare
+          </div>
+        </div>
+        <div className='info'>
+          <span>Vendor: LTP Threads</span>
+          <span>Product Type: Men's Blazer</span>
+          <span>Tags: Men, Blazer, Jacket, Top</span>
+        </div>
+        <hr />
+        <div className='details'>
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
     </div>
   )
 }
