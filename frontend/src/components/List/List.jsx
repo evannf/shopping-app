961b1444e04d37045a559d09e9de025ebf27e80f
Evannfries <evann.fries@yahoo.com>
2023-01-04 13:41:48 -0600

    product hooks created

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './List.scss'
+import Card from '../Card/Card.jsx'
 
 const List = () => {
 
@@ -43,10 +44,14 @@ const List = () => {
         },
     ]
 
-    
+
   return (
-    <div className='list'></div>
-  )
+    <div className='list'>
+        {data?.map(item => (
+            <Card item={item} key={item.id} />
+        ))}
+    </div>
+  ) 
 }
 
 export default List
--- frontend/src/pages/Products/Products.jsx
+++ frontend/src/pages/Products/Products.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useState, React } from 'react';
+import { useParams } from 'react-router-dom';
+import List from '../../components/List/List';
+import './Products.scss';  
 
 const Products = () => {
+
+  const catId = parseInt(useParams().id)
+  const [maxPrice, setMaxPrice] = useState(500)
+  const [sort, setSort] = useState(null)
+
   return (
     <div className='products'>
       <div className='left'>
@@ -23,25 +31,27 @@ const Products = () => {
           <h2>Filter by Price</h2>
           <div className='inputItem'>
             <span>$0</span>
-            <input type='range' min={0} max={500} />
-            <span>$500</span>
+            <input type='range' min={0} max={500} onChange={(e) => setMaxPrice(e.target.value)}/>
+            <span>${maxPrice}</span>
           </div>
 
         </div>
         <div className='filterItem'>
           <h2>Sort by</h2>
           <div className='inputItem'>
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input type="radio" id="asc" value="asc" name="price" onChange={e => setSort('asc')} />
             <label htmlFor='asc'> Price (Lowest First) </label>
           </div>
           <div className='inputItem'>
-            <input type="radio" id="desc" value="desc" name="price" />
+            <input type="radio" id="desc" value="desc" name="price" onChange={e => setSort('desc')} />
             <label htmlFor='desc'> Price (Highest First) </label>
           </div>
         </div>
       </div>
       <div className='right'>
         <img className='catImg' src='https://media.istockphoto.com/id/1214667734/photo/looking-directly-up-at-the-abstract-and-modern-skyline-of-the-financial-district-in-central.jpg?s=612x612&w=0&k=20&c=XGN4aga3Z3D8syHOOapKfD5K61-xqQT5OCJk-Sr5boc=' alt='products header' />
+
+        <List catId={catId} maxPrice={maxPrice} sort={sort}/>
       </div>
     </div>
   )
