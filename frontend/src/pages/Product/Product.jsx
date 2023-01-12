8136bb49aa8308b9ee40eea071049a0af6db4273
Evannfries <evann.fries@yahoo.com>
2023-01-12 14:25:28 -0600

    single page

--- frontend/src/pages/Product/Product.jsx
+++ frontend/src/pages/Product/Product.jsx
@@ -1,27 +1,40 @@
-import { useState, React } from 'react';
+import { useState ,React } from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 import "./Product.scss";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantitiy, setQuantitiy] = useState(1);
 
-  const images = [
-    "https://media.istockphoto.com/id/1152838910/photo/male-dark-blue-blazer-on-isolated-background.jpg?b=1&s=170667a&w=0&k=20&c=op8HLIk7SWCyUHJAJJ9XrcR0qVYntOBidNRhotcYKMs=",
-    "https://media.istockphoto.com/id/1332649527/photo/blank-blazer-mockup-mens-grey-suit-front-view.jpg?b=1&s=170667a&w=0&k=20&c=_jFSNrfL7DflmIq0oT8eFsucxgHvuGgbUVyMgDeOiYk="
-  ]
+  const {data, loading, error} = useFetch(
+    `/products/${id}?populate=*`
+    );
+
 
   return (
     <div className='product'>
+    {loading ? (
+      "Loading..."
+    ) : (
+      <>
       <div className='left'>
         <div className='images'>
-          <img src={images[0]} alt='' onClick={e => setSelectedImg(0)} />
-          <img src={images[1]} alt='' onClick={e => setSelectedImg(1)} />
+          <img 
+            src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url} 
+            alt='' 
+            onClick={e => setSelectedImg("")} />
+          <img 
+            src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url} 
+            alt='' 
+            onClick={e => setSelectedImg("img2")} />
         </div>
         <div className='mainImg'>
-          <img src={images[selectedImg]} alt='' />
+          <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg]?.data?.attributes?.url} alt='' />
         </div>
       </div>
       <div className='right'>
@@ -59,6 +72,7 @@ const Product = () => {
           <span>FAQ</span>
         </div>
       </div>
+      </>)}
     </div>
   )
 }
