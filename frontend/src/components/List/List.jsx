7a0bc242e06e60ece95d9339af9a7124afc29d21
Evannfries <evann.fries@yahoo.com>
2023-01-12 13:12:20 -0600

    sort methods

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -5,14 +5,17 @@ import useFetch from '../../hooks/useFetch'
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
 
-  const {data, loading, error} = useFetch(`/products?populate=*&[filters][categories][id]=${catId}`);
+  const {data, loading, error} = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map((item)=>`&[filters][sub_categories][id][$eq]=${item}`)}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+    );
 
 
   return (
     <div className='list'>
         {loading
           ? "Loading..."
-          : data?.map((item) =><Card item={item} key={item.id} />)}
+          : data?.map((item) =><Card item={item} key={item.id} />)
+        }
     </div>
   ) 
 }
