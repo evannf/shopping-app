1d2bff16410f2fbb4b3fdc58988dac6a3700b563
Evannfries <evann.fries@yahoo.com>
2023-02-02 12:45:33 -0600

    add to cart changes

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -5,24 +5,31 @@ import { useSelector } from "react-redux";
 
 const Cart = () => {
     const products = useSelector(state=>state.cart.products)
+    const totalPrice = () => {
+        let total = 0;
+        products.forEach((item)=> {
+            total += item.quantity * item.price;
+        });
+        return total.toFixed(2)
+    }
 
   return (
     <div className='cart'>
         <h1>Your Cart:</h1>
-        {products.map((item) => (
+        {products?.map((item) => (
             <div className='item' key={item.id}>
                 <img src={process.env.REACT_APP_UPLOAD_URL+item.img} alt='' />
                 <div className='details'>
                     <h1>{item.title}</h1>
                     <p>{item.desc?.substring(0,100)}</p>
-                    <div className='price'>1 x ${item.price}</div> 
+                    <div className='price'>{item.quantity} x ${item.price}</div> 
                 </div>
                 <HighlightOffOutlinedIcon className='delete'/>
             </div>
         ))}
         <div className='total'>
             <span>Subtotal:</span>
-            <span>$123</span>
+            <span>${totalPrice()}</span>
         </div>
         <button>Checkout</button>
         <span className='reset'>Reset Cart</span>
