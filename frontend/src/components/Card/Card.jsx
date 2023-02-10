88d8b7ea2c22c65e03b5306e0decd136dbda3ff8
Evannfries <evann.fries@yahoo.com>
2023-02-09 20:30:56 -0600

    data share debug

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -9,10 +9,10 @@ const Card = ({ item }) => {
             <div className='image'>
                 {item?.attributes.isNew && <span>New!</span>}
                 <img src={
-                  process.env.REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url}
+                  process.env.REACT_APP_UPLOAD_URL + item.attributes.img.data.attributes.url}
                   alt='' className='firstImg' />
                 <img src={
-                  process.env.REACT_APP_UPLOAD_URL + item.attributes?.img2?.data?.attributes?.url} 
+                  process.env.REACT_APP_UPLOAD_URL + item.attributes.img2.data.attributes.url} 
                   alt='' className='secondImg' />
             </div>
             <h2>{item?.attributes.title}</h2>
