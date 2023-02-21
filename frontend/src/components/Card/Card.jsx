84ab2f170097916ce83abd8cbb346dafa8dd28aa
Evannfries <evann.fries@yahoo.com>
2023-02-21 09:34:32 -0600

    env change

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -3,6 +3,8 @@ import './Card.scss'
 import { Link } from "react-router-dom";
 
 const Card = ({ item }) => {
+  console.log(item)
+
   return (
     <Link className='link' to = {`/product/${item.id}`}>
         <div className='card'>
